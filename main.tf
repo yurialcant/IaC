85bcadbccff737ced44dfe1a3566c6db1842dd36
Yuri Souza <102321564+yurialcant@users.noreply.github.com>
2023-07-13 18:54:04 -0300

    Adicionando o código

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+   region     = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-024e6efaf93d85776"
+  instance_type = "t2.micro"
+  key_name = "XX"
+ # user_data = <<-EOF
+                 #!/bin/bash
+ #                cd /home/ubuntu
+  #               echo "<h1>Realizado com Terraform</h1>" > index.html
+  #               nohup busybox httpd -f -p 8080 &                
+  #               EOF
+  tags = {
+    Name = "Realizando o teste na aws"
+  }
+}
+
